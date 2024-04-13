b2541e33372c3540e23a46665b857c9061a27058
azhilus <tomar3007shivansh@gmail.com>
2024-04-13 06:58:23 +0530

    deskvr

--- src/Laptop.jsx
+++ src/Laptop.jsx
@@ -1,61 +1,40 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Html, useGLTF } from "@react-three/drei";
-import { XR, useXR } from '@react-three/xr';
+import { XR } from '@react-three/xr';
 import { Environment } from '@react-three/drei';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Import OrbitControls
 import { extend, useThree, useFrame } from '@react-three/fiber';
 import YouTube from 'react-youtube'; // Import react-youtube
+import { MeshBasicMaterial, Euler } from 'three';
 
 // Extend OrbitControls to make it compatible with R3F
 extend({ OrbitControls });
 
 const CameraControls = () => {
     const { camera, gl } = useThree();
-    const { isPresenting } = useXR();
     const controls = useRef();
 
-    useFrame(() => {
-        if (!isPresenting && controls.current) {
-            controls.current.update();
-        }
-    });
+    useFrame(() => controls.current.update());
 
-    return isPresenting ? null : <orbitControls ref={controls} args={[camera, gl.domElement]} />;
+    return <orbitControls ref={controls} args={[camera, gl.domElement]} />;
 };
 
-const DeviceOrientationControls = ({ camera }) => {
-    const [orientation, setOrientation] = useState({ alpha: 0, beta: 0, gamma: 0 });
-
-    useEffect(() => {
-        const handleOrientationChange = event => {
-            setOrientation({
-                alpha: event.alpha || 0,
-                beta: event.beta || 0,
-                gamma: event.gamma || 0
-            });
-        };
-
-        window.addEventListener('deviceorientation', handleOrientationChange);
-
-        return () => {
-            window.removeEventListener('deviceorientation', handleOrientationChange);
-        };
-    }, []);
-
-    useFrame(() => {
-        const { beta, gamma } = orientation;
-
-        camera.rotation.x = beta * Math.PI / 180;
-        camera.rotation.y = gamma * Math.PI / 180;
-    });
-
-    return null;
+const RoundedPlane = ({ position, size, rotation }) => {
+    return (
+        <mesh position={position} rotation={rotation}>
+            <planeGeometry args={[size[0], size[1], 32, 32]} />
+            <meshBasicMaterial color="white" transparent opacity={1} />
+        </mesh>
+    );
 };
 
 export default function Laptop() {
-    const laptop = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
+    const iframeWidth = 800; // Width of the iframe
+    const iframeHeight = 352; // Height of the iframe
+    const iframePosition = [-3.1, 0.02, -4.5]; // Position of the iframe
+    const planeRotation = new Euler(-0.25, 0, 0); // Plane rotation
 
-    const { camera } = useThree();
+    const laptop = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
 
     return (
         <XR>
@@ -78,15 +57,15 @@ export default function Laptop() {
                 <YouTube videoId="M2QTwirp-B8" />
             </Html>
             <Html wrapperClass="laptop"
-                position={[-3.1, 0.02, -4.5]} // Adjusted position for the third iframe
+                position={iframePosition} // Adjusted position for the third iframe
                 transform
                 distanceFactor={1.2}
                 rotation-x={-0.25}
             >
-                <iframe src="https://open.spotify.com/embed/playlist/2GhYIVzJQCPZnEcqXBXnIw?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe src="https://open.spotify.com/embed/playlist/2GhYIVzJQCPZnEcqXBXnIw?utm_source=generator" width={iframeWidth} height={iframeHeight} frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </Html>
+            <RoundedPlane position={iframePosition} size={[iframeWidth / 265, iframeHeight / 172.5]} rotation={planeRotation} />
             <CameraControls />
-            <DeviceOrientationControls camera={camera} />
         </XR>
     );
 }
